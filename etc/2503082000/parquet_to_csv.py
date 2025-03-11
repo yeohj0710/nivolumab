739862fdbff868b4e9b781ec16b0f139202f0a7d
yeohj0710 <yeohj0710@yonsei.ac.kr>
2025-03-11 10:09:25 +0900

    2503111009

--- etc/2503082000/parquet_to_csv.py
+++ etc/2503082000/parquet_to_csv.py
@@ -4,29 +4,14 @@ import numpy as np
 import csv
 from tqdm import tqdm
 
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-parquet_path = os.path.join(script_dir, "..", "processed_data.parquet")
-csv_path = os.path.join(script_dir, "..", "processed_data.csv")
-
-
+parquet_path = os.path.join(script_dir, "..", "..", "processed_data.parquet")
+csv_path = os.path.join(script_dir, "..", "..", "processed_data.csv")
 np.set_printoptions(threshold=np.inf)
-
-
 df = pd.read_parquet(parquet_path)
-
-
 tqdm.pandas(ncols=50)
-
-
 if "CP_sequence" in df.columns:
-    df["CP_sequence"] = df["CP_sequence"].progress_apply(
-        lambda x: np.array2string(
-            x, threshold=np.inf, separator=" ", max_line_width=100000
-        )
-    )
-
-
+    cp_expanded = pd.DataFrame(df["CP_sequence"].tolist())
+    cp_expanded.columns = [f"CP_{i}" for i in range(cp_expanded.shape[1])]
+    df = df.drop("CP_sequence", axis=1).join(cp_expanded)
 df.to_csv(csv_path, index=False, quoting=csv.QUOTE_NONE, escapechar="\\")
